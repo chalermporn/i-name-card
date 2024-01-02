d648ee04216c029f7f50adf7458f7cf30aca9d3a
Chalermporn Posoppitakwong <chalermporn.posoppitakwong@krungthai.com>
2024-01-02 19:00:38 +0700

    fix: update eslint follow antfu

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,17 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125">
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">
+          Home
+        </RouterLink>
+        <RouterLink to="/about">
+          About
+        </RouterLink>
       </nav>
     </div>
   </header>
@@ -21,10 +24,11 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+        header {
+
+          line-height: 1.5;
+          max-height: 100vh;
+        }
 
 .logo {
   display: block;
