635f0dc26f5bd26860a328f84328fabde6182c55
Chalermporn Posoppitakwong <chalermporn.posoppitakwong@krungthai.com>
2024-01-03 14:03:25 +0700

    feat: tailwindcss config

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-3xl font-bold text-red-400 underline">
+      This is an about page
+    </h1>
+    <div class="flex h-24 p-3 m-4 border-2 border-gray-300 lg:p-4 lg:m-4">
+      dddd
+    </div>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
     display: flex;
     align-items: center;
+    min-height: 100vh;
   }
 }
 </style>
