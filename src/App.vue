28c452ed74f965b8b669293c5bbd586049c8227f
Chalermporn Posoppitakwong <chalermporn.posoppitakwong@krungthai.com>
2024-01-06 23:04:48 +0700

    update: package.json

--- src/App.vue
+++ src/App.vue
@@ -24,11 +24,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-        header {
-
-          line-height: 1.5;
-          max-height: 100vh;
-        }
+header {
+  max-height: 100vh;
+  line-height: 1.5;
+}
 
 .logo {
   display: block;
@@ -37,9 +36,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 nav {
   width: 100%;
+  margin-top: 2rem;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
@@ -63,8 +62,9 @@ nav a:first-of-type {
 @media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
     padding-right: calc(var(--section-gap) / 2);
+
+    place-items: center;
   }
 
   .logo {
@@ -73,17 +73,17 @@ nav a:first-of-type {
 
   header .wrapper {
     display: flex;
-    place-items: flex-start;
     flex-wrap: wrap;
+
+    place-items: flex-start;
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
     padding: 1rem 0;
     margin-top: 1rem;
+    margin-left: -1rem;
+    font-size: 1rem;
+    text-align: left;
   }
 }
 </style>
