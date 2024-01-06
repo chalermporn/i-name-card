d35dfd164e1759044224eeaec100d5691b8cfb74
Chalermporn Posoppitakwong <chalermporn.posoppitakwong@krungthai.com>
2024-01-06 19:14:55 +0700

    fix: router

---
+++ docs/assets/AboutView-Z1w2C8xp.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as s}from"./index-1gFIqBiM.js";const c={},a={class:"about"},n=s("h1",{class:"text-3xl font-bold text-red-400 underline"}," This is an about page ",-1),_=[n];function r(d,i){return t(),o("div",a,_)}const f=e(c,[["render",r]]);export{f as default};
